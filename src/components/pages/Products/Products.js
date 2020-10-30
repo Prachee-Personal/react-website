6e72fe66b09f2e64b899a39a7882a105892c4917
unknown <pracheesingh07@hotmail.com>
2020-10-30 16:35:59 +0530

    react website

---
+++ src/components/pages/Products/Products.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import HeroSection from '../../HeroSection';
+import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from './Data';
+import Pricing from '../../Pricing';
+
+function Products() {
+  return (
+    <>
+      <Pricing/>
+      <HeroSection {...homeObjThree} />
+      <HeroSection {...homeObjTwo} />
+    </>
+  );
+}
+
+export default Products;
